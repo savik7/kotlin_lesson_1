21ecc18d85987f6603e918c1f123452f497944e5
savik7 <viktor.savchenko00@gmail.com>
2023-01-29 09:08:33 +0300

    kotlin_lesson_2

--- app/src/main/java/com/savik/kotlin_lesson_1/MainActivity.kt
+++ app/src/main/java/com/savik/kotlin_lesson_1/MainActivity.kt
@@ -1,51 +1,24 @@
 package com.savik.kotlin_lesson_1
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private var number:Byte = 0 //-128 до 127 занимает 1 байт
-    private var number1:Short = 0 //
-    private var number2:Int = 0
-    private var number3:Long = 0
-    private var text:String = "whervbil vekbfwl ieurviuvl wkjev4iluh"
-    private var ch:Char = 'f'
-
+    private var number:Int = 123
+    private var text:String = "В магазине <осталось> $number яблока, а может и больше кто знает"
     private var tvText: TextView? = null
-    private var cLayout: ConstraintLayout? = null
-    private var counter:Int = 0
-    private var start:Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         tvText = findViewById(R.id.tvText)
-        tvText?.setText(number.toString())
+        var subText:String = text.substringAfter('<')
+        var ssubText:String = subText.substringBefore('>')
+        tvText?.setText(ssubText)
 
-        cLayout = findViewById(R.id.cLayout)
 
-        Thread{
-            start = true
-            while (start){
-                Thread.sleep(1000)
-                    runOnUiThread {
-                        if (counter == 5) cLayout?.setBackgroundColor(Color.GREEN)
-                        if (counter == 10) cLayout?.setBackgroundColor(Color.GRAY)
-                        if (counter == 15) cLayout?.setBackgroundColor(Color.RED)
-                        tvText?.setText(counter.toString())
-                        counter++
-                    }
-            }
-        }.start()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        start = false
-    }
 }
