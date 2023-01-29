22342ef8980ead8b1cf1e34c6f4e59dfc0ab3fa6
savik7 <viktor.savchenko00@gmail.com>
2023-01-29 12:14:48 +0300

    kotlin_lesson_2_home

--- app/src/main/java/com/savik/kotlin_lesson_1/MainActivity.kt
+++ app/src/main/java/com/savik/kotlin_lesson_1/MainActivity.kt
@@ -7,16 +7,22 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     private var number:Int = 123
-    private var text:String = "В магазине <осталось> $number яблока, а может и больше кто знает"
+    private var text:String = "В магазине <осталось> $number яблока, а может и больше <кто> знает"
     private var tvText: TextView? = null
+    private var tvText2: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tvText = findViewById(R.id.tvText)
+        tvText2 = findViewById(R.id.tvText2)
         var subText:String = text.substringAfter('<')
         var ssubText:String = subText.substringBefore('>')
         tvText?.setText(ssubText)
+
+        var subText2:String = text.substringAfterLast('<')
+        var ssubText2:String = subText2.substringBeforeLast('>')
+        tvText2?.setText(ssubText2)
 
 
     }
